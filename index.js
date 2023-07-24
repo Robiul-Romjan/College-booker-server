96918cf260ce8cf6bc922282060bf5a15ebbe8a3
robiul-romjan <robiulromjan7@gmail.com>
2023-07-25 02:36:51 +0600

    create colleges and user api

--- index.js
+++ index.js
@@ -16,6 +16,76 @@ const corsOptions = {
 
 
 
+
+
+  
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vhjeumo.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+
+    const usersCollection = client.db('collegeBooker').collection('users');
+    const collegesCollection = client.db('collegeBooker').collection('colleges');
+
+
+    //get all users
+    app.get("/users", async (req, res) => {
+        const result = await usersCollection.find().toArray();
+        res.send(result);
+      })
+      // save user Email and name in DB
+      app.post("/users", async (req, res) => {
+        const user = req.body;
+        const query = { email: user.email }
+        const existingUser = await usersCollection.findOne(query)
+        if (existingUser) {
+          return res.send({ message: "User already exists" })
+        }
+        const result = await usersCollection.insertOne(user);
+        res.send(result);
+      });
+
+     //get all colleges
+     app.get("/colleges", async (req, res) => {
+        const result = await collegesCollection.find().toArray();
+        res.send(result);
+      })
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+
+
+
   app.get('/', (req, res) => {
     res.send('College Booking Server is running..')
   })
